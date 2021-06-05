c02b56392ae0546f33df041e13185d58a9ba2807
VictoriaS <viccisviri@gmail.com>
2021-06-05 21:26:53 +0300

    edited readme

--- sweets/sweets/settings.py
+++ sweets/sweets/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = '3)@+@2d7bdl@qb3-#@6(z!tdt)5b7)r5=@cg)@vgg6+kgw7avd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 # Application definition
 
